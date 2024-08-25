61bfa6731448a6cbcaaafdbf5714b2de39662302
Solomon Ayuba <solomon.ayuba@miva.edu.ng>
2024-08-26 00:42:30 +0100

    Building a Guessing Game

--- main.py
+++ main.py
@@ -263,7 +263,35 @@ print("Done with loop")
 
 print(topic_sep)
 
-#Building a Guessing Game
+# Building a Guessing Game
+#simple
+# secret_word = "wiseman"
+# guess = ""
+
+# while guess != secret_word:
+#    guess = input("Guess the secret word: ")
+
+# print("You win! Secret word is \"" + secret_word + "\" ")
+
+#advanced version
+secret_word = "wiseman"
+guess = ""
+guess_count = 0
+guess_limit = 4
+out_of_guesses = False
+
+while guess != secret_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Guess the secret word: ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
+
+if out_of_guesses:
+    print("Sorry, you're out of guesses!")
+else:
+    print("You win! Secret word is \"" + secret_word + "\" ")
+
 print(topic_sep)
 
 #For Loops
