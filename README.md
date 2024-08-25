d6db3f4bff52443fcceae3f649bbec93437ede34
Solomon Ayuba <solomon.ayuba@miva.edu.ng>
2024-08-25 02:00:59 +0100

    Genesis 1

---
+++ README.md
@@ -0,0 +1,4 @@
+# Python for Dummies
+Documenting, journaling my python learning journey and making available the steps for future python dummies.
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+# Crash course with freeCodeCamp https://youtu.be/rfscVS0vtbw
+
+# Baby steps - hello world
+print("hello world")
+
+# Drawing a shape
