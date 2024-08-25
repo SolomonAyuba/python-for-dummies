8282b4650acd18539da947e47ad074d5beea805e
Solomon Ayuba <solomon.ayuba@miva.edu.ng>
2024-08-25 02:25:06 +0100

    Genesis 1

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 # Crash course with freeCodeCamp https://youtu.be/rfscVS0vtbw
 
-# Baby steps - hello world
+# 1. Babysteps: Using the Print Statement
 print("hello world")
 
-# Drawing a shape
+topic_separator = "======================"
+print(topic_separator)
+
+# Drawing a Shape
+print("     /|")
+print("    / |")
+print("   /  |")
+print("  /___|")
+
+print(topic_separator)
+
+# Variables and Datatypes
+character_name = "Sowl"
+character_age = 19
+is_male = True
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " 70 years. ")
+character_name = "Brown"
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + " 70.")
+
+print(topic_separator)
+
+# Working with Strings
+
+
+print(topic_separator)
