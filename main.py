d823e1e65910f8f209244aefbcc0ebd4b9c355e0
Solomon Ayuba <solomon.ayuba@miva.edu.ng>
2024-08-27 13:34:53 +0100

    Building a Translator

--- main.py
+++ main.py
@@ -346,8 +346,30 @@ for row in number_grid:
 print(topic_sep)
 
 # Building a Translator
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
 
+print(translate(input("Enter a word: ")))
+#modified
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation += "S"
+            else:
+                translation += "s"
+        else:
+            translation += letter
+    return translation
 
+print(translate(input("Enter a phrase: ")))
 
 print(topic_sep)
 
